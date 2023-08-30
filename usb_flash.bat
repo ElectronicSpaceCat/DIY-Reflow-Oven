1a59711242e7ff7f897f69eaae69b28efb02c1d8
AGreen <35854346+ElectronicSpaceCat@users.noreply.github.com>
2023-08-29 21:55:17 -0500

    Update usb_flash.bat

--- usb_flash.bat
+++ usb_flash.bat
@@ -3,15 +3,16 @@
 
 ECHO OFF
 
+COMM_PORT := COM4
+
 ::By setting the COM port to a different baud it effectively kills the arduino comms, allowing bossac.exe to talk to it
 
-MODE COM4:1200,N,8,1,P
+MODE $(COMM_PORT):1200,N,8,1,P
 
 ::Now erase (-e), write (-w), verify (-v), set to boot from flash (-b), and reset (-R)
 
 PROJ_DIR   := ./
 BOSSAC_DIR := $(PROJ_DIR)\Arduino15\packages\arduino\tools\bossac\1.6.1-arduino
-COMM_PORT  := COM4
 BIN_FILE   := $(PROJ_DIR)\diy_reflow_app.bin
 
 $(BOSSAC_DIR)\bossac.exe --port=$(COMM_PORT) -U false -e -w -b $(BIN_FILE) -R
