cbad56531dfb569652ca8f83abaab035289b010d
green <electronic.space.cat@gmail.com>
2023-09-02 19:35:10 -0500

    added usb_flash.sh for linux users

---
+++ usb_flash.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script is used to flash the reflow oven firmware on the Arduino Mega 2560 Rev3
+# in the event the CMSIS DAP failed to work (ie the drag-drop firmware interface)
+
+COMM_PORT="ttyACM1"
+
+#By setting the COM port to a different baud it effectively kills the arduino comms, allowing bossac.exe to talk to it
+
+stty -F /dev/$COMM_PORT 1200
+
+# Erase (-e), write (-w), verify (-v), set to boot from flash (-b), and reset (-R)
+
+PROJ_DIR=.
+BOSSAC_DIR=$PROJ_DIR/arduino-flash-tools/tools_linux_64/bossac/bin
+BIN_FILE=$PROJ_DIR/build/diy_reflow_app.bin
+
+$BOSSAC_DIR/bossac -p $COMM_PORT -U false -e -w -b $BIN_FILE -R
